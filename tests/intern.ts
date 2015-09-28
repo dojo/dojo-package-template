7e312d4b7b3f579d15429e28693ab5a095cc5a78
Kitson Kelly <me@kitsonkelly.com>
2015-09-28 20:48:05 +0100

    Changes for Intern 3

--- tests/intern.ts
+++ tests/intern.ts
@@ -10,7 +10,8 @@ export var proxyUrl = 'http://localhost:9000/';
 // Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 // automatically
 export var capabilities = {
-	'browserstack.selenium_version': '2.45.0',
+	'browserstack.selenium_version': '2.46.0',
+	'browserstack.debug': false,
 	project: 'Dojo 2',
 	name: 'dojo-<< package-name >>'
 };
@@ -19,16 +20,10 @@ export var capabilities = {
 // OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 // capabilities options specified for an environment will be copied as-is
 export var environments = [
-	{ browser: 'IE', browser_version: '11', os: 'WINDOWS', os_version: '8.1' },
-	{ browser: 'IE', browser_version: '10', os: 'WINDOWS', os_version: '8' },
-	{ browser: 'IE', browser_version: '9', os: 'WINDOWS', os_version: '7' },
-	{ browser: 'Firefox', os: 'WINDOWS', os_version: '8.1' },
-	{ browser: 'Firefox', os: 'WINDOWS', os_version: 'XP' },
-	{ browser: 'Firefox', os: 'OS X' },
-	{ browser: 'Chrome', os: 'WINDOWS', os_version: '8.1' },
-	{ browser: 'Chrome', os: 'WINDOWS', os_version: 'XP' },
-	{ browser: 'Chrome', os: 'OS X' },
-	{ browser: 'Safari', browser_version: '8', os: 'OS X' }
+	{ browserName: 'internet explorer', version: [ '9', '10', '11' ], platform: 'WINDOWS' },
+	{ browserName: 'firefox', os: 'WINDOWS' },
+	{ browserName: 'chrome', os: 'WINDOWS' },
+	{ browserName: 'Safari', version: '8', os: 'OS X' }
 ];
 
 // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
@@ -37,16 +32,9 @@ export var maxConcurrency = 2;
 // Name of the tunnel class to use for WebDriver tests
 export var tunnel = 'BrowserStackTunnel';
 
-// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
-// loader
-export var useLoader = {
-	'host-node': 'dojo/dojo',
-	'host-browser': 'node_modules/dojo/dojo.js'
-};
-
 // Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
 // can be used here
-export var loader = {
+export var loaderOptions = {
 	// Packages that should be registered with the loader in each testing environment
 	packages: [
 		{ name: 'src', location: '_build/src' },
