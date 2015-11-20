b7f8cd13168ac6b8f5c513e335735ffcfa3a8d93
Matt Wistrand <matt@troglodytidae.com>
2015-11-20 15:13:35 -0600

    Add code coverage/remapping; refactor grunt tasks.

--- tests/intern.ts
+++ tests/intern.ts
@@ -1,7 +1,9 @@
-export var proxyPort = 9000;
+import intern = require('intern');
+
+export const proxyPort = 9000;
 
 // A fully qualified URL to the Intern proxy
-export var proxyUrl = 'http://localhost:9000/';
+export const proxyUrl = 'http://localhost:9000/';
 
 // Default desired capabilities for all environments. Individual capabilities can be overridden by any of the
 // specified browser environments in the `environments` array below as well. See
@@ -9,7 +11,7 @@ export var proxyUrl = 'http://localhost:9000/';
 // https://saucelabs.com/docs/additional-config#desired-capabilities for Sauce Labs capabilities.
 // Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 // automatically
-export var capabilities = {
+export const capabilities = {
 	'browserstack.selenium_version': '2.46.0',
 	'browserstack.debug': false,
 	project: 'Dojo 2',
@@ -19,7 +21,7 @@ export var capabilities = {
 // Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
 // OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 // capabilities options specified for an environment will be copied as-is
-export var environments = [
+export const environments = [
 	{ browserName: 'internet explorer', version: [ '9', '10', '11' ], platform: 'WINDOWS' },
 	{ browserName: 'firefox', os: 'WINDOWS' },
 	{ browserName: 'chrome', os: 'WINDOWS' },
@@ -27,26 +29,42 @@ export var environments = [
 ];
 
 // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
-export var maxConcurrency = 2;
+export const maxConcurrency = 2;
 
 // Name of the tunnel class to use for WebDriver tests
-export var tunnel = 'BrowserStackTunnel';
+export const tunnel = 'BrowserStackTunnel';
+
+// Support running unit tests from a web server that isn't the intern proxy
+export const initialBaseUrl: string = (function () {
+	if (typeof location !== 'undefined' && location.pathname.indexOf('__intern/') > -1) {
+		return '/';
+	}
+	return null;
+})();
+
+// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
+// loader
+export const loaders = {
+	'host-browser': 'node_modules/dojo-loader/loader.js',
+	'host-node': 'dojo-loader'
+};
 
 // Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
 // can be used here
-export var loaderOptions = {
+export const loaderOptions = {
 	// Packages that should be registered with the loader in each testing environment
 	packages: [
 		{ name: 'src', location: '_build/src' },
-		{ name: 'tests', location: '_build/tests' }
+		{ name: 'tests', location: '_build/tests' },
+		{ name: 'dojo', location: 'node_modules/intern/node_modules/dojo' }
 	]
 };
 
 // Non-functional test suite(s) to run in each browser
-export var suites = [ 'tests/unit/all' ];
+export const suites = [ 'tests/unit/all' ];
 
 // Functional test suite(s) to run in each browser once non-functional tests are completed
-export var functionalSuites = [ 'tests/functional/all' ];
+export const functionalSuites = [ 'tests/functional/all' ];
 
 // A regular expression matching URLs to files that should not be included in code coverage analysis
-export var excludeInstrumentation = /(?:node_modules|bower_components|tests)[\/\\]/;
+export const excludeInstrumentation = /(?:node_modules|bower_components|tests)[\/\\]/;
